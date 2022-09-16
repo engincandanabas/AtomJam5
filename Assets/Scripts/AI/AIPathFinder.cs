1de8d85194e05beb4b214c8f8cc81c0d77eafbe6
Engincan Danabaş <dnbsengincan@gmail.com>
2022-09-16 20:32:32 +0300

    Kapı Sistemi Kaldırıldı

--- Assets/Scripts/AI/AIPathFinder.cs
+++ Assets/Scripts/AI/AIPathFinder.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class AIPathFinder : MonoBehaviour
 {
-    [SerializeField]private List<Door> doors = new List<Door>();
     private List<Room> rooms = new List<Room>();
 
     [SerializeField] private float speed;
@@ -13,7 +12,6 @@ public class AIPathFinder : MonoBehaviour
     private Vector3 target;
     void Start()
     {
-        doors = PathManager.instance._doors;
         rooms = PathManager.instance._rooms;
         SetFirstTarget();
     }
@@ -34,91 +32,11 @@ public class AIPathFinder : MonoBehaviour
     }
     void SetFirstTarget()
     {
-        if(doors.Count>1)
-        {
-            var tempTarget = doors[0];
-            for(int i= 1; i < doors.Count; i++)
-            {
-                if(Vector3.Distance(doors[i].transform.position,transform.position)<Vector3.Distance(tempTarget.transform.position,transform.position))
-                {
-                    // en yakin kapiyi bulma islemi
-                    tempTarget = doors[i];
-                }
-            }
-            // gecilme sayisini artir
-            tempTarget.OpenCount++;
-            // en kisa bulundu atama islemini yap
-            target = tempTarget.transform.parent.position;
-        }
+        
     }
     void SetTarget()
     {
-        Room _currentRoom= GetRoom(target, radius);
-        target = Vector3.zero;
-        // listeyi resetle
-        doors = new List<Door>();
-
-        // mevcut odanýn kapýlarýný bul
-        for (int i=0;i<_currentRoom.transform.childCount;i++)
-        {
-            if(_currentRoom.transform.GetChild(i).CompareTag("Door"))
-            {
-                //mevcut odanin kapilarini listeye ata
-                doors.Add(_currentRoom.transform.GetChild(i).GetComponent<Door>());
-            }
-        }
-        // target belirle
-        if(doors.Count>0)
-        {
-            var tempTarget=doors[0];
-            // en az gecilme sayisina sahip olani bulma
-            for(int i=0;i<doors.Count;i++)
-            {
-                if(doors[i].OpenCount< tempTarget.OpenCount)
-                {
-                    tempTarget = doors[i];
-                }
-            }
-            // gecilme sayisini artir
-            tempTarget.OpenCount++;
-            // en az gecilme sayisina sahip kapi bulundu
-            Room _selectedRoom=null;
-            var rightRoom = GetRoom(tempTarget.transform.position + transform.right * 1.5f, radius);
-            var leftRoom = GetRoom(tempTarget.transform.position + transform.right * -1.5f, radius);
-            var upRoom = GetRoom(tempTarget.transform.position + transform.up * 1.5f, radius);
-            var bottomRoom = GetRoom(tempTarget.transform.position + transform.up * -1.5f, radius);
-            if (rightRoom != null && rightRoom != _currentRoom)
-            {
-                _selectedRoom= rightRoom;
-                target = rightRoom.transform.position;
-            }
-            else if (leftRoom != null && leftRoom != _currentRoom)
-            {
-                _selectedRoom = leftRoom;
-                target = leftRoom.transform.position;
-            }
-            else if (upRoom != null && upRoom != _currentRoom)
-            {
-                _selectedRoom = upRoom;
-                target = upRoom.transform.position;
-            }
-            else if (bottomRoom != null && bottomRoom != _currentRoom)
-            {
-                _selectedRoom = bottomRoom;
-                target = bottomRoom.transform.position;
-            }
-            if(target != null)
-            {
-                for(int i=0;i<_selectedRoom.transform.childCount;i++)
-                {
-                    if(_selectedRoom.transform.GetChild(i).tag=="Door"&& Vector3.Distance(target, _selectedRoom.transform.GetChild(i).transform.position)<0.3f)
-                    {
-                        Debug.Log("Çok yakýn bir kapý bulundu.");
-                        _selectedRoom.transform.GetChild(i).GetComponent<Door>().OpenCount++;
-                    }
-                }
-            }
-        }
+        
     }
     Room GetRoom(Vector2 center, float radius)
     {
--- Assets/Scripts/Managers/PathManager.cs
+++ Assets/Scripts/Managers/PathManager.cs
@@ -5,22 +5,12 @@ using UnityEngine;
 public class PathManager : MonoBehaviour
 {
     public static PathManager instance;
-    public List<Door> _doors;
     public List<Room> _rooms;
     public Room _currentRoom;
 
     private void Awake()
     {
         instance = this;
-
-        _doors = new List<Door>();
-        GameObject[] _doorTag = GameObject.FindGameObjectsWithTag("Door");
-        for(int i=0;i< _doorTag.Length; i++)
-        {
-            _doors.Add(_doorTag[i].GetComponent<Door>());
-            _doors[i].OpenCount = 0;
-        }
-
         _rooms = new List<Room>();
         GameObject[] _roomTag = GameObject.FindGameObjectsWithTag("Room");
         for (int i = 0; i < _roomTag.Length; i++)
