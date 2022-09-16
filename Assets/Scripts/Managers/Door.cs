619a6b02db884a68328ad038144a36400a380434
Engincan Danabaş <dnbsengincan@gmail.com>
2022-09-16 20:12:00 +0300

    Revert "First Upload"
    This reverts commit 0bac4752a611d16616e8686b7d43cfedfcff29e2.

--- Assets/Scripts/Managers/Door.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Door : MonoBehaviour
-{
-    private int _openCount;
-    public int OpenCount { get { return _openCount; } set { _openCount = value; } }
-}
